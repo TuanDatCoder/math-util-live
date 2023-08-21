6fc1bc9b3f607d9e54399c886c6d3bd7a2838262
TuanDatCoder <tuandatdq03@gmail.com>
2023-08-21 19:29:33 +0700

    Lan thu 2 up len

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -20,6 +20,10 @@ public class MathUtilLive {
         System.out.println("5! = 120; actual: "+result);
         System.out.println("6! = 720; actual: "+MathUtil.getFactorial(6));
         System.out.println("0! = 1; actual: "+MathUtil.getFactorial(0));
+        
+        // them code sau lan dau tien code len server  - code len server chieu nay
+        System.out.println("1! = 1; actual: "+MathUtil.getFactorial(1));
+        System.out.println("3! = 6; actual: "+MathUtil.getFactorial(3));
     }
     
 }
